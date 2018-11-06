c0d9838ec146e68cd0c616f0bb25fa5ecc33dc67
alumno <alumno@aula1-01>
2018-11-06 18:39:53 +0100

    inicio de agenda

---
+++ agenda/src/agenda/Agenda.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agenda;
+
+/**
+ *
+ * @author alumno
+ */
+public class Agenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+Persona per1 = new Persona("juan","111");
+Persona per2 = new Persona("pepe", "222", 22);
+    Persona[] vAgenda = new Persona[5];
+     
+    }
+    
+}
---
+++ agenda/src/agenda/Persona.java
@@ -0,0 +1,70 @@
+package agenda;
+
+/**
+ *
+ * @author dra69
+ */
+public class Persona {
+    private String nombre;
+    private String telefono;
+    private int edad;
+
+    public Persona(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        edad = 0;
+    }
+
+    public Persona(String nombre, String telefono, int edad) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.edad = edad;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the telefono
+     */
+    public String getTelefono() {
+        return telefono;
+    }
+
+    /**
+     * @param telefono the telefono to set
+     */
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    /**
+     * @return the edad
+     */
+    public int getEdad() {
+        return edad;
+    }
+
+    /**
+     * @param edad the edad to set
+     */
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String toString() {
+        return  "nombre=" + nombre + ", telefono=" + telefono + ", edad=" + edad ;
+    }
+ 
+}
